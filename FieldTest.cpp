df6e1ad193b62bbd729ff67c84668bd6e03e6a7c
Jake Magers <jmagers1@mail.csuchico.edu>
2016-09-26 11:49:43 -0700

    Add tests for isSafe and revealAdjacent

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,59 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+
+TEST(FieldTest, isSafeFalse)
+{
+	Field minefield;
+	
+	minefield.placeMine(3,3);
+	ASSERT_FALSE( minefield.isSafe(3,3) );
+}
+
+TEST(FieldTest, isSafeTrue)
+{
+	Field minefield;
+	
+	minefield.placeMine(3,4);
+	ASSERT_TRUE( minefield.isSafe(3,3) );
+}
+
+TEST(FieldTest, revealAdjacentCenter)
+{
+	Field minefield;
+  minefield.revealAdjacent(2,2);
+	
+	ASSERT_EQ( EMPTY_SHOWN, minefield.get(2,2) );
+}
+
+TEST(FieldTest, revealAdjacentRight)
+{
+	Field minefield;
+  minefield.revealAdjacent(2,2);
+
+	ASSERT_EQ( EMPTY_SHOWN, minefield.get(3,2) );
+}
+
+TEST(FieldTest, revealAdjacentLeft)
+{
+	Field minefield;
+  minefield.revealAdjacent(2,2);
+
+	ASSERT_EQ( EMPTY_SHOWN, minefield.get(1,2) );
+}
+
+TEST(FieldTest, revealAdjacentAbove)
+{
+	Field minefield;
+  minefield.revealAdjacent(2,2);
+
+	ASSERT_EQ( EMPTY_SHOWN, minefield.get(2,1) );
+}
+
+TEST(FieldTest, revealAdjacentBellow)
+{
+	Field minefield;
+  minefield.revealAdjacent(2,2);
+
+	ASSERT_EQ( EMPTY_SHOWN, minefield.get(2,3) );
+}
